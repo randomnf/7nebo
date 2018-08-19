4541a4e281dea8e1b7c45689a991daf2a2923a2e
randomnf <therandom@ya.ru>
2018-08-19 19:09:29 +0300

    Initial commit

---
+++ js/simple-carousel-bg.js
@@ -0,0 +1,15 @@
+// var sliderBG = document.querySelector(".action-slider");
+// var indexBG = 0;
+//
+// carouselBG();
+//
+// function carouselBG(){
+// 	for(var i=1;i<=actionSliderImagesCount;i++){
+// 		sliderBG.classList.remove("action-slider_bg-"+i);
+// 	}
+// 	indexBG++;
+// 	if(indexBG>actionSliderImagesCount)
+// 		indexBG=1;
+// 	sliderBG.classList.add("action-slider_bg-"+indexBG);
+// 	setTimeout(carouselBG, 5000);
+// }
